17bb3f2accee3859549865f3922d3488a455a25a
Sean Guo <swguonc@gmail.com>
2025-04-11 13:36:22 -0700

    all box ohio data

--- toy_python_model/ohio_data.py
+++ toy_python_model/ohio_data.py
@@ -6,13 +6,22 @@ from tqdm import tqdm
 from math_functions import *
 from parameters import *
 
-ohio_data = np.genfromtxt("data_100.csv", delimiter=",")
+def show_data(data, start_time, end_time):
+    print(data.shape)
+    fig, axes = plt.subplots(2, 3)
+    for i in range(axes.size-1):
+        x_i, y_i = i // 3, i % 3
+        plt.sca(axes[x_i, y_i])
+        plt.plot(ohio_data[i,start_time:end_time])
+    
+    plt.suptitle(f"t = {start_time} to {end_time}")
+    plt.show()
+
+
+
+ohio_data = np.genfromtxt("C:\\Users\\swguo\\VSCode Projects\\Heart Modeling\\Ohio\\data_101.csv", delimiter=",")
 print(ohio_data.shape)
-fig, axes = plt.subplots(2, 3)
-for i in range(axes.size-1):
-    x_i, y_i = i // 3, i % 3
-    plt.sca(axes[x_i, y_i])
-    plt.plot(ohio_data[i,0:10000])
 
+show_data(ohio_data, 0, 10000)
+show_data(ohio_data, 0, 2500)
 
-plt.show()
